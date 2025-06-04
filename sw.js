f02a6b9a771fe01dbdca8b5c76d991cc0fd6627f
AmirAli0088 <amirali.abd0088@gmail.com>
2025-06-05 00:13:12 +0330

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,25 @@
-// sw.js
-self.addEventListener('install', (e) => {
-  console.log('Service Worker نصب شد.');
-  self.skipWaiting();
+// این فایل باید در ریشه سایت قرار گیرد (همان دایرکتوری که فایل HTML قرار دارد)
+self.addEventListener('install', event => {
+    console.log('Service Worker نصب شد');
+    self.skipWaiting();
 });
 
-self.addEventListener('activate', (e) => {
-  console.log('Service Worker فعال شد.');
+self.addEventListener('activate', event => {
+    console.log('Service Worker فعال شد');
 });
 
-self.addEventListener('push', (e) => {
-  const data = e.data.json();
-  self.registration.showNotification(data.title, {
-    body: data.body,
-    icon: data.icon,
-  });
+self.addEventListener('notificationclick', event => {
+    event.notification.close();
+    event.waitUntil(
+        clients.matchAll({type: 'window'}).then(windowClients => {
+            for (let client of windowClients) {
+                if (client.url === '/' && 'focus' in client) {
+                    return client.focus();
+                }
+            }
+            if (clients.openWindow) {
+                return clients.openWindow('/');
+            }
+        })
+    );
 });
